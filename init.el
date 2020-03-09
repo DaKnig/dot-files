2ffd321f720c52d002a5a9a920e5bf62dbafbf59
DaKnig <37626476+DaKnig@users.noreply.github.com>
2020-03-09 17:31:12 +0200

    a few minor addition

--- init.el
+++ init.el
@@ -8,9 +8,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(desktop-save-mode t)
  '(package-selected-packages
    (quote
-    (company-anaconda auto-package-update company-irony-c-headers irony company-irony company dashboard centaur-tabs beacon mark-multiple use-package buffer-expose avy monokai-theme))))
+    (latex-mode company-anaconda auto-package-update company-irony-c-headers irony company-irony company dashboard centaur-tabs beacon mark-multiple use-package buffer-expose avy monokai-theme))))
 ;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
