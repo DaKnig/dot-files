d70c9644af4be39828b93a8b1fd375458925772f
DaKnig <37626476+DaKnig@users.noreply.github.com>
2023-01-28 10:00:15 +0200

    more robust config. slight theme change.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,8 @@
 
 
 
-(when (file-readable-p "~/.emacs.d/config.org")
-  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(when (file-readable-p (concat user-emacs-directory "config.org"))
+  (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org"))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,4 +22,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(region ((t (:inherit highlight :background "dim gray")))))
+ '(region ((t (:inherit highlight :background "gray")))))
